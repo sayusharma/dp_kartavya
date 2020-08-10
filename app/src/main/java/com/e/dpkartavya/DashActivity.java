bc1f078e23b2d8e651d3cc00035e2190d1f76f71
Ayush Sharma <31429182+sayusharma@users.noreply.github.com>
2020-08-10 14:15:40 +0530

    Half of Basic Details completed

--- app/src/main/java/com/e/dpkartavya/DashActivity.java
+++ app/src/main/java/com/e/dpkartavya/DashActivity.java
@@ -2,13 +2,47 @@ package com.e.dpkartavya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
-public class DashActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class DashActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private customAdapter adapter;
+    private Spinner locationspinner;
+    private ArrayList<CustomItem> customList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash);
+        locationspinner = findViewById(R.id.locationspinner);
+        customList = getCustomList();
+        adapter = new customAdapter(this, customList);
+        locationspinner.setAdapter(adapter);
+        locationspinner.setOnItemSelectedListener(this);
+    }
+    public void onClickVerify(View view)
+    {
+        Intent intent = new Intent(DashActivity.this,VerifyActivity.class);
+        startActivity(intent);
+    }
+    private ArrayList<CustomItem> getCustomList() {
+        customList = new ArrayList<>();
+        customList.add(new CustomItem("Select  Police Station"));
+        customList.add(new CustomItem("Vasant Vihar"));
+        return customList;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
